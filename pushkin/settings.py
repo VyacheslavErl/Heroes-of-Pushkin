c1d08fd0a802961be23c2240e16e6e3f085ae370
Văĉeslav <121387441+VyacheslavErl@users.noreply.github.com>
2024-01-15 02:54:55 +0400

    Update settings.py

--- pushkin/settings.py
+++ pushkin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://65a45df23a1518cb88597fb5--heroes-of-pushkin.netlify.app/']
 
 # Application definition
 
